ba7e5c6b0ab6c17d642c964fbb97b682bc8acee8
Albert Nguyen <ndttam.dev@gmail.com>
2022-03-25 10:52:17 +0700

    preapre for test

---
+++ src/shared/modules/UserProfile/UserProfile.tsx
@@ -0,0 +1,11 @@
+import AppLayout from 'core/layouts/app-layout';
+import React from 'react';
+import './UserProfile.scss';
+
+const UserProfile = (props?: any) => {
+    return <AppLayout>
+        <div> User profile Display here .....</div>
+    </AppLayout>
+}
+
+export default UserProfile;
