d7941c4ce709e53fc9967c937f107ef2b218c2ea
Albert Nguyen <ndttam.dev@gmail.com>
2022-03-31 23:41:02 +0700

    added another login methods

--- src/shared/modules/AnotherMethodLogin/AnotherMethodLogin.tsx
+++ src/shared/modules/AnotherMethodLogin/AnotherMethodLogin.tsx
@@ -1,8 +1,11 @@
-import { AppBar, DialogProps, IconButton, Slide, Toolbar } from '@mui/material';
+import { AppBar, DialogProps, IconButton, List, Slide, Toolbar, Typography, Card } from '@mui/material';
 import Dialog from '@mui/material/Dialog';
 import { TransitionProps } from '@mui/material/transitions';
-import React from 'react';
+import React, { useState } from 'react';
 import CloseIcon from '@mui/icons-material/Close';
+import loginMethods from './methods.json';
+import MethodItem from './MethodItem/MethodItem';
+import styled from 'styled-components';
 
 type AnotherMethodLoginType = {
 
@@ -19,12 +22,48 @@ const Transition = React.forwardRef(function Transition(
     return <Slide direction="up" ref={ref} {...props} />;
 });
 
+export interface MethodType {
+    id: string,
+    name: string,
+    type: string,
+    icon?: string,
+    hot?: boolean | false
+}
+
+const ListStyled = styled(List)`
+    background-color: black; 
+    position: relative;
+    width: 100%;
+    height: 100%;
+    align-items: center;
+    justify-content: center;
+    display:flex;
+    flex-direction: row;
+`
+
 const AnotherMethodLogin = (props: AnotherMethodLoginType, ref: React.Ref<HTMLDivElement>) => {
+
+    const [methods] = useState(() => {
+        let result: MethodType[] = []
+        loginMethods.map((m) => {
+            let item: MethodType = {
+                ...m
+            };
+
+            result.push(item);
+        });
+
+        return result;
+    });
     const onClose = () => { }
     return <Dialog fullScreen open={props?.open || false} onClose={onClose} TransitionComponent={Transition} ref={ref}>
-        <AppBar>
+        <AppBar sx={{ position: 'relative' }}>
             <Toolbar>
-                <IconButton edge="end"
+                <Typography sx={{ ml: 2, flex: 1 }} component="div">
+                    {/* NOTE: Let's it empty */}
+                </Typography>
+
+                <IconButton edge="start"
                     color="inherit"
                     onClick={props.handleClose}
                     arial-label="close"
@@ -33,7 +72,15 @@ const AnotherMethodLogin = (props: AnotherMethodLoginType, ref: React.Ref<HTMLDi
                 </IconButton>
             </Toolbar>
         </AppBar>
-        {props?.children}</Dialog>;
+        {props?.children}
+
+        <ListStyled sx={{ pt: 0 }}>
+            {
+                methods.map((method) => {
+                    return <MethodItem key={method.id} data={method}></MethodItem>
+                })}
+        </ListStyled>
+    </Dialog>;
 }
 
 const AnotherMethodLoginRef = React.forwardRef(AnotherMethodLogin);
